97d7c6e715fe56089667499bb04786eee9d8f834
santhosh thangavel <santhoshk.thangavel@gmail.com>
2021-08-24 09:45:29 +0100

    Single inheritance

---
+++ src/classes-and-object-orientation/single_inheritance.py
@@ -0,0 +1,23 @@
+class Base:
+    def __init__(self):
+        print("Base initializer")
+
+    def f(self):
+        print("Base.f()")
+
+
+class Sub(Base):
+    def __init__(self):
+        super().__init__()
+        print("Sub initializer")
+
+    def f(self):
+        print("Sub.f()")
+
+
+b = Base()
+print(b.f())
+
+
+s = Sub()
+print(s.f())
