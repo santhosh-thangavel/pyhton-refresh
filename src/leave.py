9e2685d112c7afb2932874d55fbf58e0bc56c46b
santhosh thangavel <santhoshthangavel@Santhoshs-MacBook-Pro.local>
2022-08-01 17:27:55 +0100

    simple function

---
+++ src/leave.py
@@ -0,0 +1,10 @@
+def leave(name, days, year):
+    print(f"Dear {name}\nyou have {days} of leave balance for this\nYear {year}")
+
+
+leave("kim", 15, 2022)
+
+# output
+# Dear kim
+# you have 15 of leave balance for this
+# Year 2022
