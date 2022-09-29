27de484b7cda72e3ee29bea449a190ad91143aaf
santhosh thangavel <santhoshk.thangavel@gmail.com>
2022-09-29 12:28:42 +0100

    How to bind global into local scope

--- src/functions_and_functional_programming/closures.py
+++ src/functions_and_functional_programming/closures.py
@@ -1,3 +1,28 @@
+# message = "global"
+#
+#
+# def enclosing():
+#     message = "enclosing"
+#
+#     def local():
+#         message = "local"
+#         print("local message", message)
+#
+#     print("Enclosing message", message)
+#     local()
+#     print("Enclosing message", message)
+#
+#
+# print("Global message", message)
+# enclosing()
+# print("Global message", message)
+
+# Answer:
+# Global message global
+# Enclosing message enclosing
+# local message local
+# Enclosing message enclosing
+
 message = "global"
 
 
@@ -5,14 +30,24 @@ def enclosing():
     message = "enclosing"
 
     def local():
+        global message  #we can bind global message into local.
+                        # Here global modifies the binding in local
         message = "local"
-        print("local message", message)
+        # print("local message", message)
 
     print("Enclosing message", message)
     local()
     print("Enclosing message", message)
 
 
-print("Global message", message)
+print("Global message:1", message)
 enclosing()
 print("Global message", message)
+
+# Answer:-
+
+# Global message:1 global
+# Enclosing message enclosing
+# Enclosing message enclosing
+# Global message local
+
