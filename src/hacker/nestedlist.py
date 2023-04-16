5d1118c7b9e739e8017a0db87f291a4b22a3dc5c
santhosh thangavel <santhoshk.thangavel@gmail.com>
2023-04-16 11:03:41 +0100

    marks and students

--- src/hacker/nestedlist.py
+++ src/hacker/nestedlist.py
@@ -23,32 +23,3 @@ if __name__ == '__main__':
                 names.append(record[0])
 
     print(names)
-
-
-
-
-    # result = []
-
-    # for student in students:
-    #     result.append(student[1])
-    # # print(students)
-    # print(result)
-
-    # first = math.inf
-    # second = math.inf
-    # # print(first)
-    # for i in range(0, len(result)):
-    #     if first > result[i]:
-    #         first = result[i]
-    # # print(first)
-    # for i in range(0, len(result)):
-    #     if first != result[i] and second > result[i]:
-    #         second = result[i]
-    # print(second)
-    # # print(type(second))
-
-    # # for x in result:
-    # #     if second in x:
-    # #          print(x)
-
-    # # print(list(enumerate(students[1])))
