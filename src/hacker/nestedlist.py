7f4d8114efc10c5d4c55413943d70e5015353c5c
santhosh thangavel <santhoshk.thangavel@gmail.com>
2023-04-23 11:13:09 +0100

    2nd lowest without algorithms

--- src/hacker/nestedlist.py
+++ src/hacker/nestedlist.py
@@ -7,19 +7,20 @@ if __name__ == '__main__':
         records.append([name, score])
         scores.append(score)
 
-    print(records)
+    # print(records)
     # print(scores)
 
     sorted_score = list(sorted(set(scores), reverse=True))
-    print(sorted_score)
-    print(sorted_score[-2])
+    # print(sorted_score)
+    # print(sorted_score[-2])
 
     names =[]
     for record in records:
-        print(record)
+        # print(record)
         for value in record:
-            print(value)
-            if value == sorted_score[-2]:
+            # print(value)
+            if value == sorted_score[0]:
                 names.append(record[0])
 
-    print(names)
+    names = sorted(names)
+    print(*names,sep="\n")
